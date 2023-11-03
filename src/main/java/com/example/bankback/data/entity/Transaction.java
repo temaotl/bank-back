7b54d4f49a2ac6e5359d0fbdb1f445538c3a7b46
Artem Otliaguzov <temaotl0992@gmail.com>
2023-11-03 19:29:05 +0100

    make Transaction

--- src/main/java/com/example/bankback/data/entity/Transaction.java
+++ src/main/java/com/example/bankback/data/entity/Transaction.java
@@ -1,4 +1,119 @@
 package com.example.bankback.data.entity;
 
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Entity
+@Table(name = "transactions")
 public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private LocalDateTime date;
+
+    @Column(nullable = true)
+    private String senderAccountNumber;
+
+    @Column(nullable = true)
+    private String receiverAccountNumber;
+
+    @Column(nullable = false)
+    private BigDecimal amount;
+
+    @Column(nullable = false, length = 3)
+    private String currency;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+
+    public String getSenderAccountNumber() {
+        return senderAccountNumber;
+    }
+
+    public void setSenderAccountNumber(String senderAccountNumber) {
+        this.senderAccountNumber = senderAccountNumber;
+    }
+
+    public String getReceiverAccountNumber() {
+        return receiverAccountNumber;
+    }
+
+    public void setReceiverAccountNumber(String receiverAccountNumber) {
+        this.receiverAccountNumber = receiverAccountNumber;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Transaction that = (Transaction) o;
+
+        if (!Objects.equals(id, that.id)) return false;
+        if (!Objects.equals(date, that.date)) return false;
+        if (!Objects.equals(senderAccountNumber, that.senderAccountNumber))
+            return false;
+        if (!Objects.equals(receiverAccountNumber, that.receiverAccountNumber))
+            return false;
+        if (!Objects.equals(amount, that.amount)) return false;
+        return Objects.equals(currency, that.currency);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (date != null ? date.hashCode() : 0);
+        result = 31 * result + (senderAccountNumber != null ? senderAccountNumber.hashCode() : 0);
+        result = 31 * result + (receiverAccountNumber != null ? receiverAccountNumber.hashCode() : 0);
+        result = 31 * result + (amount != null ? amount.hashCode() : 0);
+        result = 31 * result + (currency != null ? currency.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Transaction{" +
+                "id=" + id +
+                ", date=" + date +
+                ", senderAccountNumber='" + senderAccountNumber + '\'' +
+                ", receiverAccountNumber='" + receiverAccountNumber + '\'' +
+                ", amount=" + amount +
+                ", currency='" + currency + '\'' +
+                '}';
+    }
 }
+
