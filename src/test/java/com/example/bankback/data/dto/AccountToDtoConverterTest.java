2312f0cd1bd3094604f3bcaa2c70d8a70569fdd4
Artem Otliaguzov <temaotl0992@gmail.com>
2023-11-03 23:36:33 +0100

    make and test converters between account and Account Dto

---
+++ src/test/java/com/example/bankback/data/dto/AccountToDtoConverterTest.java
@@ -0,0 +1,72 @@
+package com.example.bankback.data.dto;
+
+import com.example.bankback.data.dto.converters.AccountToDtoConverter;
+import com.example.bankback.data.dto.converters.DtoToAccountConverter;
+import com.example.bankback.data.entity.Account;
+import com.example.bankback.data.entity.User;
+import com.example.bankback.data.repository.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Transactional
+class AccountToDtoConverterTest {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private AccountToDtoConverter accountToDtoConverter;
+
+    @Autowired
+    private DtoToAccountConverter dtoToAccountConverter;
+
+    @Test
+    void testAccountToDtoAndDtoToAccountConversion() {
+        User newUser = new User();
+        newUser.setFirstName("Kirin");
+        newUser.setLastName("Dorian");
+        newUser.setBirthDate(LocalDate.of(1990, 5, 12));
+        newUser.setPassport("123456789");
+        newUser.setPhoneNumber("+1234567890");
+        newUser.setEmail("kirin.dorian@example.com");
+        newUser.setAddress("123 Main St");
+
+        User savedUser = userRepository.save(newUser);
+
+
+        Account newAccount = new Account();
+        newAccount.setName("Savings Account");
+        newAccount.setAccountNumber("123455");
+        newAccount.setIBAN("CZ123456789");
+        newAccount.setBalance(new BigDecimal("1000.00"));
+        newAccount.setCurrency("CZK");
+        newAccount.setUser(savedUser);
+
+
+        AccountDTO accountDTO = accountToDtoConverter.apply(newAccount);
+        assertThat(accountDTO.getUserId()).isEqualTo(savedUser.getId());
+        assertThat(accountDTO.getAccountNumber()).isEqualTo(newAccount.getAccountNumber());
+        assertThat(accountDTO.getIBAN()).isEqualTo(newAccount.getIBAN());
+        assertThat(accountDTO.getName()).isEqualTo(newAccount.getName());
+        assertThat(accountDTO.getBalance()).isEqualByComparingTo(newAccount.getBalance());
+        assertThat(accountDTO.getCurrency()).isEqualTo(newAccount.getCurrency());
+
+
+        Account convertedAccount = dtoToAccountConverter.apply(accountDTO);
+        assertThat(convertedAccount.getUser()).isEqualTo(savedUser);
+        assertThat(convertedAccount.getAccountNumber()).isEqualTo(accountDTO.getAccountNumber());
+        assertThat(convertedAccount.getIBAN()).isEqualTo(accountDTO.getIBAN());
+        assertThat(convertedAccount.getName()).isEqualTo(accountDTO.getName());
+        assertThat(convertedAccount.getBalance()).isEqualByComparingTo(accountDTO.getBalance());
+        assertThat(convertedAccount.getCurrency()).isEqualTo(accountDTO.getCurrency());
+    }
+}
+
