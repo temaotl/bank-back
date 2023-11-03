50156709dbde350be82675d80702c135e29b8ad0
Artem Otliaguzov <temaotl0992@gmail.com>
2023-11-03 14:01:24 +0100

    start

---
+++ src/main/java/com/example/bankback/BankBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.bankback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankBackApplication.class, args);
+    }
+
+}
