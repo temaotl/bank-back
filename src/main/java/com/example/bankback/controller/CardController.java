2ac5dd5f082f11e2451bf46f9ac4c5701b1adbb9
Artem Otliaguzov <temaotl0992@gmail.com>
2023-11-09 15:02:05 +0100

    create and tested on postman CardController

---
+++ src/main/java/com/example/bankback/controller/CardController.java
@@ -0,0 +1,55 @@
+package com.example.bankback.controller;
+
+
+
+import com.example.bankback.business.CardService;
+import com.example.bankback.data.dto.CardDTO;
+import com.example.bankback.data.dto.converters.CardToDtoConverter;
+import com.example.bankback.data.dto.converters.DtoToCardConverter;
+import com.example.bankback.data.entity.Card;
+import com.example.bankback.data.repository.CardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+import javax.validation.Valid;
+
+
+@RestController
+@RequestMapping("/cards")
+public class CardController extends AbstractCrudController<Card, CardDTO, Long, CardRepository> {
+
+    private final  CardService service;
+
+    @Autowired
+    protected CardController(CardService service,
+                             CardRepository repository,
+                             CardToDtoConverter toDtoConverter,
+                             DtoToCardConverter toEntityConverter) {
+        super(repository, toDtoConverter, toEntityConverter);
+        this.service = service;
+    }
+
+
+    @Override
+    @PostMapping
+    public ResponseEntity<CardDTO> create(@Valid @RequestBody CardDTO dto) {
+        CardDTO createdCard = service.create(dto);
+        return new ResponseEntity<>(createdCard, HttpStatus.CREATED);
+    }
+
+    @Override
+    @PutMapping("/{id}")
+    public ResponseEntity<CardDTO> update(@Valid @RequestBody CardDTO dto, @PathVariable Long id) {
+        try {
+            service.update(dto, id);
+            Card updatedCard = toEntityConverter.apply(dto);
+            return ResponseEntity.ok(toDtoConverter.apply(updatedCard));
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+    }
+}
+
